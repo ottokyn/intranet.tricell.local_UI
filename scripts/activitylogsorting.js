226c04e57f985e76a57cbdac721719e5cf2886bc
ottokyn <otto.kyntaja@ha.ax>
2026-04-07 14:04:52 +0300

    Delete scripts directory

--- scripts/activitylogsorting.js
+++
@@ -1,7 +0,0 @@
-function sorting(number, type2)
-{
-    document.cookie = "sortAmount=" + number + "; path=/";
-    document.cookie = "sortType=" + type2 + "; path=/";
-
-    window.location.href = "/api/activitylog";
-}
--- scripts/leftmenu.js
+++
@@ -1,8 +0,0 @@
-function openMenu() 
-{
-	document.getElementById("leftsidebar").style.width = "220px";
-}
-function closeMenu() 
-{
-	document.getElementById("leftsidebar").style.width = "0";
-}
